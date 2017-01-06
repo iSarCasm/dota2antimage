5166e4918ea866d105688e06ae2b588d0f286708
SarCasm <sarcasm008@gmail.com>
2017-01-07 00:31:10 +0300

    Removed useless crap

--- dev/utility.lua
+++ dev/utility.lua
@@ -75,64 +75,6 @@ function M:GetNearBySuccessorPointOnLane(Lane,Location)
     return PointsOnLane[0];
 end
 
-function M.IsItemAvailable(item_name)
-    local npcBot = GetBot();
-    -- query item code by Hewdraw
-    for i = 0, 5, 1 do
-        local item = npcBot:GetItemInSlot(i);
-        if(item and item:GetName() == item_name) then
-            return item;
-        end
-    end
-    return nil;
-end
-
-function M:GetComfortPoint(creeps,LANE)
-    local npcBot = GetBot();
-    local mypos = npcBot:GetLocation();
-    local x_pos_sum = 0;
-    local y_pos_sum = 0;
-    local count = 0;
-    local meele_coefficient = 5;-- Consider meele creeps first
-    local coefficient = 1;
-    for creep_k,creep in pairs(creeps)
-    do
-        local creep_name = creep:GetUnitName();
-        local meleepos = string.find( creep_name,"melee");
-        if(meleepos ~= nil) then
-            coefficient = meele_coefficient;
-        else
-            coefficient = 1;
-        end
-
-        creep_pos = creep:GetLocation();
-        x_pos_sum = x_pos_sum + coefficient * creep_pos[1];
-        y_pos_sum = y_pos_sum + coefficient * creep_pos[2];
-        count = count + coefficient;
-    end
-
-    local avg_pos_x = x_pos_sum / count;
-    local avg_pos_y = y_pos_sum / count;
-
-    if(count > 0) then
-        return self:GetNearByPrecursorPointOnLane(LANE,Vector(avg_pos_x,avg_pos_y)) + RandomVector(20);
-    else
-        return nil;
-    end;
-end
-
-function M:HasEmptySlot()
-    local npcBot = GetBot();
-    -- query item code by Hewdraw
-    for i = 0, 5, 1 do
-        local item = npcBot:GetItemInSlot(i);
-        if(item == nil) then
-            return true;
-        end
-    end
-    return false;
-end
-
 function M:CourierThink()
     local npcBot = GetBot();
     for i = 9, 15, 1 do
@@ -226,7 +168,7 @@ function M:CreepGC()
         local rm = false;
         for t,_ in pairs(time_health)
         do
-            if(GameTime() - t > 60) then
+            if(GameTime() - t > 20) then
                 rm = true;
             end
             break;
@@ -295,15 +237,6 @@ function M:GetCreepHealthDeltaPerSec(creep, time)
         return 0;
     end
 
-end
-
-function M:ConsiderRunAway()
-    local npcBot = GetBot();
-    local enemy = npcBot:GetNearbyHeroes( 600, true, BOT_MODE_NONE );
-    if(#enemy > 1 and DotaTime() > 360) then
-        return true;
-    end
-    return false;
 end
 
 function M:GetEnemyBots()
