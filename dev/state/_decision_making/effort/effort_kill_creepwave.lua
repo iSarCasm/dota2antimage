e4979ccd8b8d42d20dc8e719b81c3d0afb406bb4
SarCasm <sarcasm008@gmail.com>
2017-01-12 17:02:00 +0300

    State Reward\Effort multiplayer tweaks for hero kill and farmlane

--- dev/state/_decision_making/effort/effort_kill_creepwave.lua
+++ dev/state/_decision_making/effort/effort_kill_creepwave.lua
@@ -3,8 +3,8 @@ local EffortKillCreepwave = {}
 function EffortKillCreepwave:Generic()
   local bot = GetBot();
   local creep_health = 2000; -- approx creep wave health
-  local dps = bot:GetBaseDamage();
-  local last_hit_simplify = 0.1; -- ??
+  local dps = bot:GetBaseDamage() + 50; -- needs API fix
+  local last_hit_simplify = 0.25; -- ??
   return (creep_health * last_hit_simplify) / dps;
 end
 ----------------------------------------------------
--- dev/state/_decision_making/effort/effort_kill_hero.lua
+++ dev/state/_decision_making/effort/effort_kill_hero.lua
@@ -2,11 +2,13 @@ local EffortKillHero = {}
 ----------------------------------------------------
 local UnitHelper      = require(GetScriptDirectory().."/dev/helper/unit_helper");
 ----------------------------------------------------
+EffortKillHero.Multiplayer = 5;
+----------------------------------------------------
 function EffortKillHero:Hero(Hero)
   local bot = GetBot();
   local my_power = self:Power(bot);
   local his_power = self:Power(Hero);
-  return Max(his_power - my_power, 0);
+  return Max(self.Multiplayer * (his_power - my_power), 0);
 end
 ----------------------------------------------------
 function EffortKillHero:Power(Hero)
